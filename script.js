7cb932b02c869f118b613fb20284b7f2b507519f
Saron <saronabebe2019@gmail.com>
2025-03-11 13:13:44 -0800

    Add delete and edit functionalities

--- script.js
+++ script.js
@@ -14,7 +14,6 @@ let library = JSON.parse(localStorage.getItem("library")) || [];
 displayBooks(); // Show books when page loads
 
 showDialog.addEventListener("click", () => {
-  dialog.classList.add("dialog");
   dialog.showModal();
 });
 
@@ -54,6 +53,7 @@ function displayBooks() {
     let bookCard = document.createElement("div");
     bookCard.classList.add("book-card");
 
+    let bookContent = document.createElement("div");
     let bookTitle = document.createElement("h3");
     bookTitle.textContent = book.title;
 
@@ -67,7 +67,47 @@ function displayBooks() {
     let readStatus = document.createElement("p");
     readStatus.textContent = book.isRead ? "Read" : "Not Read";
 
-    bookCard.append(bookTitle, bookAuthor, pages, readStatus);
+    // delete and edit functionalities
+
+    let buttons = document.createElement("div");
+    buttons.classList.add("book-buttons");
+    let del = document.createElement("button");
+    del.innerHTML = "delete";
+    del.addEventListener("click", () => {
+      bookCard.remove();
+      let index = library.indexOf(book);
+      library.splice(index, 1);
+      localStorage.setItem("library", JSON.stringify(library));
+    });
+
+    let edit = document.createElement("button");
+    edit.innerHTML = "edit";
+
+    edit.addEventListener("click", () => {
+      const newTitle = prompt("Enter new title:", book.title);
+      const newAuthor = prompt("Enter new author:", book.author);
+      const newPages = prompt("Enter new number of pages:", book.noOfPages);
+      const newIsRead = confirm(
+        "Have you read this book? OK = Read, Cancel = Not Read"
+      );
+
+      // Update the book object in the array
+      book.title = newTitle;
+      book.author = newAuthor;
+      book.noOfPages = newPages;
+      book.isRead = newIsRead;
+
+      // Update localStorage
+      localStorage.setItem("library", JSON.stringify(library));
+
+      // Re-render the book cards
+      displayBooks();
+    });
+
+    buttons.append(del, edit);
+
+    bookContent.append(bookTitle, bookAuthor, pages, readStatus);
+    bookCard.append(bookContent, buttons);
     content.appendChild(bookCard);
   });
 }
